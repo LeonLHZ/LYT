6c9ed67030a43d3df1c3a65a0bf7abf1d3ba9f92
LeonLHZ <760843690@qq.com>
2019-09-11 22:42:44 +0800

    init

---
+++ src/main/java/cn/lhz/lyt/LytApplication.java
@@ -0,0 +1,13 @@
+package cn.lhz.lyt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LytApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LytApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cn/lhz/lyt/LytApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.lhz.lyt;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LytApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
