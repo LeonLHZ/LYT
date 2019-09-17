a6dc6f7c8e9101d0aad912bf7c942867b37104db
LeonLHZ <760843690@qq.com>
2019-09-18 00:34:45 +0800

    配置文件更改

---
+++ src/main/java/cn/lhz/lyt/config/LytConfig.java
@@ -0,0 +1,17 @@
+package cn.lhz.lyt.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 配置类
+ *
+ * @author lhz
+ * @date 2019/9/17 22:13
+ */
+@Configuration
+public class LytConfig
+{
+    public LytConfig()
+    {
+    }
+}
